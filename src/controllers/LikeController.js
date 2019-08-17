782565dedf2ee8a1bbff919e3ee315a38ec26b3d
Victor Costa <victorcostadev@gmail.com>
2019-08-17 20:55:11 -0300

    implementing the match feature

--- src/controllers/LikeController.js
+++ src/controllers/LikeController.js
@@ -2,23 +2,37 @@ const Dev = require('../models/Dev');
 
 module.exports = {
   async store(req, res) {
-    const { user } = req.headers;
-    const { devId } = req.params;
+    const { devId } = req.params
+    const { user } = req.headers
 
-    const loggedDev = await Dev.findById(user);
-    const targetDev = await Dev.findById(devId);
+    const loggedDev = await Dev.findById(user)
+    const targetDev = await Dev.findById(devId)
 
     if (!targetDev) {
       return res.status(400).json({error: 'Dev not exists'});
     }
-
-    if (loggedDev.likes.includes(loggedDev._id)) {
-      console.log('DEU MATCH!');
-    }
-
+    
     loggedDev.likes.push(targetDev._id);
 
     await loggedDev.save();
+
+    console.log(`User ${loggedDev.user} liked ${targetDev.user}`)
+
+    if (targetDev.likes.includes(loggedDev._id)) {
+      console.log('QUE MASSA DEU MATCH!')
+      const loggedSocket = req.connectedUsers[user]
+      const targetSocket = req.connectedUsers[devId]
+
+      if (loggedSocket) {
+        req.io.to(loggedSocket).emit('match', targetDev)
+      }
+
+      if (targetSocket) {
+        req.io.to(targetSocket).emit('match', loggedDev)
+      }
+    }
+
+    
 
     return res.json(loggedDev);
 
